ea2915419e9434d09568d518cdd3c433103d61de
Alexandr98 <sashaprim98@gmail.com>
2018-08-16 19:25:10 +0300

    marketdata added and fixed errors

--- src/app/stats/store/effects/getMarketData.effect.ts
+++ src/app/stats/store/effects/getMarketData.effect.ts
@@ -1,7 +1,7 @@
 import { Actions, Effect } from '@ngrx/effects';
- import { Action } from '@ngrx/store';
+import { Action } from '@ngrx/store';
 import { Injectable } from '@angular/core';
-import {  map, switchMap, tap } from 'rxjs/operators';
+import {  map, switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { DaoService } from '../../../organization/services/dao.service';
 import * as getData  from '../actions/getRequests.action';
